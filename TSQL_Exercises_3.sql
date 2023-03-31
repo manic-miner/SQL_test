3bb742f327e406f014292fe6a96d9f3e8064b426
tomek <thetomk@gmail.com>
2023-03-31 23:38:46 +0200

    Added new exercises

--- TSQL_Exercises_3.sql
+++ TSQL_Exercises_3.sql
@@ -6,4 +6,20 @@ FROM Sales.Orders
 WHERE orderdate IN (
     SELECT MAX(o.orderdate)
     FROM Sales.Orders AS o
-)
+);
+
+-- Exercise 2
+SELECT custid, orderid, orderdate, empid
+FROM Sales.Orders AS o
+WHERE o.custid IN (
+    SELECT TOP 1 WITH TIES custid
+    FROM Sales.Orders
+    GROUP BY custid
+    ORDER BY COUNT(*) DESC
+);
+
+-- Exercise 3
+
+
+
+-- Exercise 4
