56c847bd046ff2d04a495e6968caf6eb53897f96
tomek <thetomk@gmail.com>
2023-04-01 23:31:32 +0200

    added new exercises

--- TSQL_Exercises_3.sql
+++ TSQL_Exercises_3.sql
@@ -19,7 +19,17 @@ WHERE o.custid IN (
 );
 
 -- Exercise 3
-
-
+SELECT empid, firstname, lastname
+FROM HR.Employees
+WHERE empid NOT IN (
+    SELECT empid FROM Sales.Orders
+    WHERE orderdate >= '2016-05-01'
+);
 
 -- Exercise 4
+SELECT DISTINCT country
+FROM Sales.Customers
+WHERE country NOT IN (
+    SELECT country
+    FROM HR.Employees
+)
